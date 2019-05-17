074c3757302eb9b2a908205ec510533f35a5970c
Jonathan Henrique Galdino <jhenrique87@yahoo.com.br>
2019-05-17 10:50:07 -0300

    versão 3 : laço for

--- guess_game.py
+++ guess_game.py
@@ -2,25 +2,28 @@ print("\n**************************")
 print("Bem vindo ao jogo de advinhação")
 print("*************************\n")
 
-
+tries = 3
+run = 1
 secret_game = 42
+for run in range(tries):
+    print("\nTentativa{} de {}".format(run+1,tries))
+    guess_str = input("Digite numero :")
+    guess = int(guess_str)
 
-guess_str = input("digite numero")
-guess = input(guess_str)
+    correct = guess == secret_game
+    bigger = guess > secret_game
+    smaller = guess < secret_game
 
+    if correct:
+        print("Certa Resposta")
+    else:
+        if bigger:
+            print("Erro, o seu numero é maior")
+        elif smaller:
+            print("Erro, o seu numero é menor")
 
-correct = guess == secret_game
-bigger = guess > secret_game
-smaller = guess < secret_game
+    run += 1
+    tries -= 1
 
-
-if correct:
-    print("Certa Resposta")
-
-else :
-    if bigger:
-        print("Erro, o seu numero é maior")
-    elif smaller:
-        print("Erro, o seu numero é menor")
 
 print("*********FIM DE JOGO***********")
