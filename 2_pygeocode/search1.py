078808eefcfd2dbfa8d860a75947ff8e9465af36
Jonathan Henrique Galdino <jhenrique87@yahoo.com.br>
2019-05-31 09:49:19 -0300

    Search1: operações básicas

---
+++ 2_pygeocode/search1.py
@@ -0,0 +1,8 @@
+from geopy.geocoders import Nominatim
+
+if __name__ == '__main__':
+    address = 'Belarmino Vilela Junqueira, Ituiutaba, MG'
+    user_agent = 'Search1'
+    location = Nominatim(user_agent=user_agent).geocode(address)
+    print(location.latitude, location.longitude)
+
