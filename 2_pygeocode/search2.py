e3986514fdc4c7581d927841fed23ea267516fdd
Jonathan Henrique Galdino <jhenrique87@yahoo.com.br>
2019-05-31 10:14:29 -0300

    Search3: operações básicas

---
+++ 2_pygeocode/search2.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+import requests
+
+
+def geocode(address):
+    base = 'https://nominatim.openstreetmap.org/search'
+    parameters = {'q': address, 'format': 'json'}
+    user_agent = 'Search2.py'
+    headers = {'User-Agent': user_agent}
+    response = requests.get(base, params=parameters, headers=headers)
+    reply = response.json()
+    print(reply)
+    print(reply[0]['lat'], reply[0]['lon'])
+
+
+if __name__ == '__main__':
+    geocode('Belarmino Vilela Junqueira, Ituiutaba, MG')
---
+++ 2_pygeocode/search3.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+import http.client
+import json
+from urllib.parse import quote_plus
+
+base = '/search'
+def geocode(address):
+    path = '{}?q={}&format=json'.format(base, quote_plus(address))
+    user_agent = b'Foundations of Python Network Programming example search3.py'
+    headers = {b'User-Agent': user_agent}
+    connection = http.client.HTTPSConnection('nominatim.openstreetmap.org')
+    connection.request('GET', path, None, headers)
+    rawreply = connection.getresponse().read()
+    reply = json.loads(rawreply.decode('utf-8'))
+    print(reply[0]['lat'], reply[0]['lon'])
+
+if __name__ == '__main__':
+        geocode('Belarmino Vilela Junqueira, Ituiutaba, MG')
