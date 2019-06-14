e7f7f48263e95df0c1db21036c805954870daa92
Jonathan Henrique Galdino <jhenrique87@yahoo.com.br>
2019-06-14 10:53:17 -0300

    SEARCH 2 TRATADO

--- 2_pygeocode/search2.py
+++ 2_pygeocode/search2.py
@@ -9,8 +9,13 @@ def geocode(address):
     headers = {'User-Agent': user_agent}
     response = requests.get(base, params=parameters, headers=headers)
     reply = response.json()
+    print('Endereço :', address)
+    print('Resultado 1 :')
+    print('\t\tCEP : ',reply[0]['display_name'].split(',')[7])
     print('\t\tLatitude e Longetude','(',reply[0]['lat'],',', reply[0]['lon'],')')
 
+    print('Resultado 2 :')
+    print('\t\tCEP : ', reply[1]['display_name'].split(',')[7])
     print('\t\tLatitude e Longetude','(',reply[1]['lat'],',', reply[1]['lon'],')')
 
 
