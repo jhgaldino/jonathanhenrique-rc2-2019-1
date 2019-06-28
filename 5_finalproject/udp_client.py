a920b052c5081766f77cd39461f4edbf4d0d225e
Jonathan <jhenrique87@yahoo.com.br>
2019-06-28 10:58:55 -0300

    projeto final

--- 5_finalproject/udp_client.py
+++ 5_finalproject/udp_client.py
@@ -5,3 +5,10 @@ BUFFSIZE = 65535
 def client(network, port):
     #criando o socket para internet e udp
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+    #conectando
+    sock.connect((host, port))
+
+
+if __name__ == '__main__':
+    server("", 1060)
--- 5_finalproject/udp_server.py
+++ 5_finalproject/udp_server.py
@@ -2,6 +2,26 @@ import socket
 
 BUFFSIZE = 65535
 
-
+#criando o socket
 def server(interface, port):
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    #definindo o numero de jogadores
+    players = 4
+
+    sock.bind((interface, port))
+
+    while True:
+        print('Waiting to accept a new connection')
+        # Aceita novas conexões
+        # A cada nova conexão, um socket ativo (ou connected socket)
+        # é criado e retornado na varíável sc
+        sc, sockname = sock.accept()
+        print('We have accepted a connection from', sockname)
+        print('  Socket name:', sc.getsockname())
+        print('  Socket peer:', sc.getpeername())
+
+        # Aguardando o cliente enviar os dados
+        message = recvall(sc, 16)
+        print('  Incoming sixteen-octet message:', repr(message))
+
+
